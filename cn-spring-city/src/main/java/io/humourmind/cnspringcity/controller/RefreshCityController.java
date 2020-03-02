93d457b7353a2edce706db4326fd20b9263a55b9
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-03-02 14:43:40 +0800

    Added mapper to show refresh

--- cn-spring-city/src/main/java/io/humourmind/cnspringcity/controller/RefreshCityController.java
+++ cn-spring-city/src/main/java/io/humourmind/cnspringcity/controller/RefreshCityController.java
@@ -2,6 +2,8 @@ package io.humourmind.cnspringcity.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +12,11 @@ public class RefreshCityController {
 
 	@Value("${hello.there: Hi there!}")
 	private String refreshValue;
+
+	@RequestMapping("/checkRefresh")
+	public String checkRefresh(){
+		return refreshValue;
+	}
 
 
 }
